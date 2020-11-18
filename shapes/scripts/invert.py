40469196b090f80bb5648bdebb0b32a52834ee01
Wassim <wassim.jabrane@gmail.com>
2020-11-18 00:05:05 +0000

    Fixed small error where it generated images into wrong folder. Works perfectly fine now! You can run the script on your machine too :)

---
+++ shapes/scripts/invert.py
@@ -0,0 +1,65 @@
+import cv2
+import os
+import shutil
+#Utility file to invert some of the files
+
+#Script file to generate inverted templates from existing images
+sphere_original_path = '../spheres/original'
+sphere_inverted_path = '../spheres/inverted'
+sphere_rotated_path = '../spheres/rotated'
+
+box_original_path = '../boxes/original'
+box_inverted_path= '../boxes/inverted'
+box_rotated_path=  '../boxes/rotated'
+
+#Test folder paths to generate inverted ones to pass to the classifier model
+test_original_path = '../tests/original'
+test_inverted_path = '../tests/inverted'
+
+
+
+
+
+
+def generate_inverted_pictures(initial_path,output_dr_path):
+    for filename in os.listdir(initial_path):
+        img= cv2.imread(os.path.join(initial_path,filename),0)
+        output = cv2.bitwise_not(img)
+
+        output_filename = os.path.splitext(filename)[0] + '_inverted.png'
+        print(output_filename)
+
+        cv2.imwrite(os.path.join(output_dr_path, output_filename), output)
+
+
+
+#Create a new inverted folder for spheres and delete previous one
+shutil.rmtree(sphere_inverted_path) #Delete previous inverted path file - MAKE SURE YOU DONT DELETE THE ORIGINAL
+os.mkdir(sphere_inverted_path) #Create a new inverted folder under sphere
+
+#Create a new inverted folder for boxes and delete previous one
+shutil.rmtree(box_inverted_path) #Delete previous inverted folder - MAKE SURE YOU DONT DELETE THE ORIGINAL
+os.mkdir(box_inverted_path) #Create a new inverted folder under box
+
+#Create a new inverted folder for test images and delete previous one
+shutil.rmtree(test_inverted_path) #Delete previous inverted folder - MAKE SURE YOU DONT DELETE THE ORIGINAL
+os.mkdir(test_inverted_path) #Create a new inverted folder under box
+
+
+#invert spheres from original and rotated folder. We will be using these inverted images for training
+generate_inverted_pictures(sphere_original_path,sphere_inverted_path)
+generate_inverted_pictures(sphere_rotated_path,sphere_inverted_path)
+
+#invert boxes from original and rotated folder. We will be using these inverted imagess for trainig
+generate_inverted_pictures(box_original_path,box_inverted_path)
+generate_inverted_pictures(box_rotated_path,box_inverted_path)
+
+#invert test images from original test folders - we will be using these for our classifiers as a test set.
+generate_inverted_pictures(test_original_path,test_inverted_path)
+
+
+
+
+
+
+
--- src/images_sync.py
+++ src/images_sync.py
@@ -12,6 +12,8 @@ from cv_bridge import CvBridge, CvBridgeError
 import message_filters
 
 
+import numpy.cos as cos
+import numpy.sin as sin
 
 class images_sync:
 
@@ -31,142 +33,114 @@ class images_sync:
     self.b_sub1 = message_filters.Subscriber("/image1/joint_centers/blue", Float64MultiArray)
     self.g_sub1 = message_filters.Subscriber("/image1/joint_centers/green", Float64MultiArray)
     self.r_sub1 = message_filters.Subscriber("/image1/joint_centers/red", Float64MultiArray)
+    self.target_sub1 = message_filters.Subscriber("/image1/target_center", Float64MultiArray)
 
     self.y_sub2 = message_filters.Subscriber("/image2/joint_centers/yellow", Float64MultiArray)
     self.b_sub2 = message_filters.Subscriber("/image2/joint_centers/blue", Float64MultiArray)
     self.g_sub2 = message_filters.Subscriber("/image2/joint_centers/green", Float64MultiArray)
     self.r_sub2 = message_filters.Subscriber("/image2/joint_centers/red", Float64MultiArray)
-    
+
     #initialize trajectory error
     self.error = np.array([0.0,0.0], dtype='float64') 
     self.init_time = rospy.get_time()
+    self.target_sub2 = message_filters.Subscriber("/image2/target_center", Float64MultiArray)
+
     # synchronise incoming channels using the timestamps contained in their headers
     # slop defines the delay in seconds with which messages are synchronized
     ts = message_filters.ApproximateTimeSynchronizer([
-                                                      self.y_sub1, self.b_sub1, self.g_sub1, self.r_sub1,
-                                                      self.y_sub2, self.b_sub2, self.g_sub2, self.r_sub2
-                                                     ],
+                                                      self.y_sub1, self.b_sub1, self.g_sub1, self.r_sub1, self.target_sub1,
+                                                      self.y_sub2, self.b_sub2, self.g_sub2, self.r_sub2, self.target_sub2
+                                                    ],
                                                      queue_size=10,slop= 0.1, allow_headerless=True)
     ts.registerCallback(self.callback)
 
+
+    self.previous_green = np.array([0,0,0])
+    self.initial = True
+
+
   #Note: Image 2 - xz plane; Image 1 - yz plane
-  def create_new_3d_coordinates_from_data(self,y1,b1,g1,r1,y2,b2,g2,r2):
-    self.yellow_center1 = y1.data
-    self.blue_center1 = b1.data
-    self.green_center1 = g1.data
-    self.red_center1 = r1.data
+  def create_new_3d_coordinates_from_data(self,y1,b1,g1,r1,target1,y2,b2,g2,r2,target2):
+    self.yellow_center1 = np.asarray(y1.data)
+    self.blue_center1 = np.asarray(b1.data)
+    self.green_center1 = np.asarray(g1.data)
+    self.red_center1 = np.asarray(r1.data)
+    self.target_center1 = np.asarray(target1.data)
+    print(self.target_center1)
 
-    self.yellow_center2 = y2.data
-    self.blue_center2 = b2.data
-    self.green_center2 = g2.data
-    self.red_center2 = r2.data
+    self.yellow_center2 = np.asarray(y2.data)
+    self.blue_center2 = np.asarray(b2.data)
+    self.green_center2 = np.asarray(g2.data)
+    self.red_center2 = np.asarray(r2.data)
+    self.target_center2 = np.asarray(target2.data)
+    print(self.target_center2)
 
-    self.z_center = int((self.yellow_center1[1] + self.yellow_center2[1]) / 2)
-    self.z_blue = int((self.blue_center1[1] + self.blue_center2[1]) / 2)
-    self.z_green = int((self.green_center1[1] + self.green_center2[1]) / 2)
+
+
+
+    #blue and yellow should always on the same x and y-axis:
+    self.blue_center1[0] = self.yellow_center1[0]
+    self.blue_center2[0] = self.yellow_center2[0]
+
+    distance_blue_green_link1 = np.sqrt(np.sum((self.green_center1 - self.blue_center1) ** 2))
+    distance_blue_green_link2 = np.sqrt(np.sum((self.green_center2 - self.blue_center2) ** 2))
+    if distance_blue_green_link1 >= distance_blue_green_link2:
+      self.z_center =self.yellow_center1[1]
+      self.z_blue = self.blue_center1[1]
+      self.z_green = self.green_center1[1]
+      self.z_red = self.red_center1[1]
+    else:
+      self.z_center = self.yellow_center2[1]
+      self.z_blue = self.blue_center2[1]
+      self.z_green = self.green_center2[1]
+      self.z_red = self.red_center2[1]
+      # if distance_blue_green_link2 > 92:
+      #   self.z_green = self.z_green - self.z_green*0.015
+      #   self.green_center2[0] = self.green_center2[0] - self.green_center2[0]*0.015
+
+    print(np.sqrt(np.sum((np.array([self.green_center1]) - np.array([self.blue_center1])) ** 2)))
+    print(np.sqrt(np.sum((np.array([self.green_center2]) - np.array([self.blue_center2])) ** 2)))
+
+
 
     self.yellow_3d = np.array([self.yellow_center2[0], self.yellow_center1[0], self.z_center])
     self.blue_3d = np.array([self.blue_center2[0], self.blue_center1[0], self.z_blue])
     self.green_3d = np.array([self.green_center2[0], self.green_center1[0], self.z_green])
+    self.red_3d = np.array([self.red_center2[0], self.red_center1[0], self.z_red])
 
 
   #Calculate the conversion from pixel to meter,
-  # using the circles yellow and blue and the length of the link (2.5 m) between them.
+  # using the joints green and red and the length of the link (3 m) between them.
   def pixel2meter(self):
     # find the euclidean distance between two circles
-    dist = np.sqrt(np.sum((self.yellow_3d - self.blue_3d) ** 2))
-    return 2.5 / dist
+
+    yellow_blue_link_length = np.sqrt(np.sum((self.yellow_3d - self.blue_3d) ** 2))
+
+    return 2.5/yellow_blue_link_length
 
   # TODO: Change axis for other centers as well.
   def changeAxis(self):
     new_blue_3d = np.array([0, 0, 0])
     new_green_3d = self.blue_3d - self.green_3d
     ratio = self.pixel2meter()
-    self.blue_3d = new_blue_3d * ratio
-    self.green_3d = new_green_3d * ratio
+    print("Ratio:" + str(ratio))
+    self.blue_3d = new_blue_3d * 0.0389
+    self.green_3d = new_green_3d * 0.0389
     print("Values changed to meters:")
     print("Blue:"+ str(self.blue_3d))
     print("Green" + str(self.green_3d))
 
-  #TODO: Deal with prespective issue. Angles returned may be severely inaccurate because of the camera's perspective
-  def estimate_joint_angles_brute_force_green_blue(self):
-    xs = np.linspace(-np.pi / 2, np.pi / 2, 90)
-    ys = np.linspace(-np.pi / 2, np.pi / 2, 90)
-    print("GOAL: " + str(self.green_3d))
-    original_green_3d = self.blue_3d.copy()
-    original_green_3d[2] = original_green_3d[2] + 3.5
-    print("ORIGNAL: " + str(original_green_3d))
-
-    for x in xs:
-      for y in ys:
-        R_y_then_x = np.array([
-          [np.cos(y), 0, np.sin(y)],
-          [np.sin(x) * np.sin(y), np.cos(x), -np.sin(x) * np.cos(y)],
-          [-np.cos(x) * np.sin(y), np.sin(x), np.cos(x) * np.cos(y)]
-        ])
-
-        R_x_then_y = np.array([
-          [np.cos(y), np.sin(y) * np.sin(x), np.sin(y) * np.cos(x)],
-          [0, np.cos(x), -np.sin(x)],
-          [-np.sin(y), np.cos(y) * np.sin(x), np.cos(y) * np.cos(x)]
-        ])
-
-        expected_green_yx = R_y_then_x.dot(original_green_3d)
-        expected_green_xy = R_x_then_y.dot(original_green_3d)
-
-        # if y > 0.98 and y < 1.1 and x >= -0.1 and x < 0.1:
-        #   print(x)
-        #   print(expected_green_yx)
-        #   print(expected_green_xy)
-        #   print(self.green_3d)
-
-        if np.all(abs(expected_green_yx - self.green_3d) < 0.05):
-          x=-x
-          y=-y
-          return R_y_then_x, x, y
-        if np.all(abs(expected_green_xy - self.green_3d) < 0.05):
-          x = -x
-          y = -y
-          return R_x_then_y, x, y
-
-    return None, None, None
+  def forward_kinematics(self):
+    # r =np.array([(cos(a+b+c)+cos(a-b-c))/2,	-sin(b+c),	 (sin(a+b+c)+sin(a-b-c))/,	 (5*sin(a+b+c)+5*sin(a-b-c))/4
+    #     (sin(a+b+c)-sin(a-b-c))/2	 cos(b+c)	(-cos(a+b+c)+cos(a-b-c))/2	(-5*cos(a+b+c)+5*cos(a-b-c))/4
+    #               -sin(a)	        0	                    cos(a)	                (5*cos(a)+6)/2
+    #                     0	        0	                         0	                             1
+    #
+    # ]
+    pass
 
 
-  # Find angles that the vector makes with the x,y,and z axes
-  def find_direction_angles_green_blue(self):
-    x_distance = (self.blue_center2[0] - self.green_center2[0])
-    y_distance = (self.blue_center1[0] - self.green_center1[0])
-    z_distance = (self.z_blue - self.z_green)
-    print("DISTANCE:" + str(np.array([x_distance, y_distance, z_distance])))
 
-    ax = np.arccos(x_distance / (np.sqrt(pow(x_distance, 2) + pow(y_distance, 2) + pow(z_distance, 2))))
-    ay = np.arccos(y_distance / (np.sqrt(pow(x_distance, 2) + pow(z_distance, 2) + pow(y_distance, 2))))
-    az = np.arccos(z_distance / (np.sqrt(pow(x_distance, 2) + pow(y_distance, 2) + pow(z_distance, 2))))
-    result = (self.blue_3d.dot(self.green_3d)) / (
-            np.sqrt(self.blue_3d.dot(self.blue_3d.T)) * np.sqrt(self.green_3d.dot(self.green_3d.T)))
-    angle_between_vectors = np.arccos(result)
-    print(angle_between_vectors)
-    print(np.arctan2(x_distance, z_distance))
-    # print(np.arctan2(np.linalg.norm(np.cross(center_coords,blue_coords)),np.dot(center_coords,blue_coords)))
-    print(np.array([ax - np.pi / 2, ay - np.pi / 2, az]))
-    # print(angle_between_vectors)
-
-  #Testing purposes
-  def print_rotation_results_green_blue(self):
-    angle = 0.4 + np.pi
-
-    Rx = np.array([[1, 0, 0], [0, np.cos(angle), -np.sin(angle)], [0, np.sin(angle), np.cos(angle)]])
-    Ry = np.array([[np.cos(angle), 0, np.sin(angle)], [0, 1, 0], [-np.sin(angle), 0, np.cos(angle)]])
-    # print(Rx.dot(green_3d_unit) * np.linalg.norm(green_3d) )
-    # print(Rx)
-    # print(green_3d)
-    original_green_3d = self.blue_3d.copy()
-    original_green_3d[2] = original_green_3d[2] - 3.5
-    print("Inital Green Point Coordinates: " + str(original_green_3d))
-    print("Rotation around x-axis")
-    print(Rx @ original_green_3d)
-    print("Rotation around y-axis")
-    print(Ry @ original_green_3d)
 
   def get_jacobian(self, x, y, z, w):
     xv = (float) (7*sin(y+z)+6*sin(w+y+z)-6*sin(w-y+z)+7*sin(y-z))/4
@@ -181,28 +155,30 @@ class images_sync:
   def compute_tracking_error(self, desired, actual):
     err = desired-actual
     current_time = rospy.get_time()
-    current_trajectory = np.arry([image1])
+    # current_trajectory = np.array([image1])
 
   def closed_loop_control(self, theta1, theta2, theta3, theta4):
 
 
   # Recieve data from both image_processing nodes corresponding to both cameras, process it, and publish
   # TODO: Alot needs to be changed. The processing part is incomplete. Dont forget to publish at the end too.
-  def callback(self,y1,b1,g1,r1,y2,b2,g2,r2):
+  def callback(self,y1,b1,g1,r1,target1,y2,b2,g2,r2,target2):
     print("--------------------------------------------")
 
-    self.create_new_3d_coordinates_from_data(y1,b1,g1,r1,y2,b2,g2,r2)
+    self.create_new_3d_coordinates_from_data(y1,b1,g1,r1,target1,y2,b2,g2,r2,target2)
     print("Green Center: Img Coordinates" + str(self.green_3d) )
     print("Blue Center: Img Coordinates" + str(self.blue_3d) )
 
     self.changeAxis()
 
-    print("Brute Force- ")
-    R, x, y = self.estimate_joint_angles_brute_force_green_blue()
-    print("RESULTS:")
-    print("Joint2: predicted x-rotation angle: " + str(x))
-    print("Joint3: predicted y-rotation angle:" + str(y))
-    print("Rotation Matrix" + str(R))
+    # print("Brute Force- ")
+    # R, x, y = self.estimate_joint_angles_brute_force_green_blue()
+    # print("RESULTS:")
+    # print("Joint2: predicted x-rotation angle: " + str(x))
+    # print("Joint3: predicted y-rotation angle:" + str(y))
+    # print("Rotation Matrix" + str(R))
+    # # print("Multiplying to Rotation Matrix: " + str(R@[0,0,3.5]))
+
 
 
 
